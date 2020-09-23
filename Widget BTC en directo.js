43a5f3c6dfb653736e26c8226d3cf9e52ca73736
Matias Ruiz <64507598+matiasruizmdz@users.noreply.github.com>
2020-09-23 14:47:50 -0300

    Ultima actualización precio max precio min y estilo

--- Widget BTC en directo.js
+++ Widget BTC en directo.js
@@ -5,22 +5,25 @@ const res = await req.loadJSON();
 if (config.runsInWidget) {
     const widget = new ListWidget();
 
-    const imgmati = await new Request("https://i.pinimg.com/originals/71/ba/45/71ba454512bf5f5485fbc623e1295262.png");
-    const imgm = await imgmati.loadImage();
+    const imgReqBq = await new Request("https://i.pinimg.com/originals/71/ba/45/71ba454512bf5f5485fbc623e1295262.png");
+    const imgBq = await imgReqBq.loadImage();
     
-    widget.backgroundImage = imgm;
+    widget.backgroundImage = imgBq;
 
     const title = widget.addText("PRECIO BTC");
     title.textColor = Color.red();
     title.textOpacity = 1;
-    title.font = new Font("Courier", 16);
+    title.font = new Font("Arial Black", 16);
+    title.centerAlignText()
+    
 
     widget.addSpacer(5);
 
-    const btcname = widget.addText(`VENTA:${res.ask} COMPRA:${res.bid}HIGH:${res.high}  LOW:${res.low}`);
+    const btcname = widget.addText(`VENTA:${res.ask} COMPRA:${res.bid} HIGH:${res.high}   LOW:${res.low}`);
     btcname.textColor = Color.yellow();
     btcname.textOpacity = 1;
-    btcname.font = new Font("Courier", 12);
+    btcname.font = new Font("Courier", 11);
+    btcname.centerAlignText()
 
     Script.setWidget(widget);
     Script.complete();
